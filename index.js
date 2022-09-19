0c960a3bb7f4c165219764d431366ca7d598200c
Rodrigo Taboada <rodri.taboada@gmail.com>
2022-09-19 19:57:28 -0300

    Desafio 2

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
+let notas = prompt("Ingrese SIGUIENTE si desea promediar las notas, sino ingrese NO para terminar");
 
-alert("Ingresa las notas de los estudiantes de este año y vamos a sacar el promedio y ver si aprobaron!");
-
-let promedioCalculo 
-let suma 
-let ingresaNotas 
-let ingresaNotas2 
-let ingresaNotas3 
-
-function notas(){
-ingresaNotas = Number(prompt("Ingrese la nota del 1er trimestre del alumno"));
-ingresaNotas2 = Number(prompt("Ingrese la nota del 2do trimestre del alumno"));
-ingresaNotas3 = Number(prompt("Ingrese la nota del 3er trimestre del alumno"));
-
-while ((ingresaNotas == isNaN) && (ingresaNotas2 == isNaN) && (ingresaNotas3 == isNaN)){  
-alert("Ingresa una nota")
-}
-
-}
 
 function promedio(ingresaNotas,ingresaNotas2,ingresaNotas3){
-    suma = ingresaNotas + ingresaNotas2 + ingresaNotas3;
-    promedioCalculo = suma / 3;
+    let suma = ingresaNotas + ingresaNotas2 + ingresaNotas3;
+    let promedioDeNotas = suma / 3;
+    return promedioDeNotas
 }
 
-function felicitaciones(){
-    if( promedio >= 7){
-        alert("Felicitaciones APROBASTE")
+function felicitaciones(promedioDeNotas){
+    if( promedioDeNotas >= 7){
+        alert(`Felicitaciones APROBASTE, tu promedio fue de ${promedioDeNotas}`)
     }else{
-        alert("Desaprobado, pero podes recuperarlo!");
+        alert(`Desaprobado, pero podes recuperarlo!`);
 }
 }
 
-notas()
-promedio()
-felicitaciones()
+
+while (notas != "NO"){  
+    if((notas != "SIGUIENTE") && (notas != "NO")){
+        alert("No ingresaste una respuesta valida")
+        notas = number(prompt("Ingrese calcular si desea promediar las notas, sino ingrese cancelar"));
+    }else{
+        ingresaNotas = Number(prompt("Ingrese la nota del 1er trimestre del alumno"));
+        ingresaNotas2 = Number(prompt("Ingrese la nota del 2do trimestre del alumno"));
+        ingresaNotas3 = Number(prompt("Ingrese la nota del 3er trimestre del alumno"));
+        let promedioDeNotas = promedio(ingresaNotas, ingresaNotas2, ingresaNotas3)
+        felicitaciones(promedioDeNotas)
+        notas = number(prompt("Ingrese calcular si desea promediar las notas, sino ingrese cancelar"));
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
