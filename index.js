2192e7479cee3082d8a48cbc917c951f0f282f08
Rodrigo Taboada <rodri.taboada@gmail.com>
2022-09-27 18:13:42 -0300

    cards centradas y agregue mas

--- index.js
+++ index.js
@@ -43,6 +43,46 @@ let monitores = [
         resolucion: 27,
         precio: 120000, 
         imagen : "https://http2.mlstatic.com/D_NQ_NP_665931-MLA45212708615_032021-O.webp"        
+},
+{
+        id: 6,
+        marca: "Viewsonic",
+        modelo: "Vx2768",
+        resolucion: 27,
+        precio: 100499, 
+        imagen : "https://http2.mlstatic.com/D_NQ_NP_614055-MLA50623198260_072022-O.webp"        
+},
+{
+        id: 7,
+        marca: "Viewsonic",
+        modelo: "Xg2431",
+        resolucion: 24,
+        precio: 115869, 
+        imagen : "https://http2.mlstatic.com/D_NQ_NP_665087-MLA49041346064_022022-O.webp"        
+},
+{
+        id: 8,
+        marca: "Samsung",
+        modelo: "F24T35",
+        resolucion: 24,
+        precio: 54599, 
+        imagen : "https://http2.mlstatic.com/D_NQ_NP_973781-MLA48131216539_112021-O.webp"        
+},
+{
+        id: 9,
+        marca: "Samsung",
+        modelo: "Odyssey G3",
+        resolucion: 24,
+        precio: 70699, 
+        imagen : "https://http2.mlstatic.com/D_NQ_NP_926028-MLA48705312123_122021-O.webp"        
+},
+{
+        id: 10,
+        marca: "LG",
+        modelo: "29WP500",
+        resolucion: 29,
+        precio: 89900, 
+        imagen : "https://http2.mlstatic.com/D_NQ_NP_818868-MLA48049743458_102021-O.webp"        
 }
 ]
 
@@ -59,9 +99,9 @@ monitores.forEach(item => {
     <div class="card" style="width: 18rem;">
         <img src="${item.imagen}" class="card-img-top" alt="Gadnic">
     <div class="card-body">
-        <h5 class="card-text">${item.marca}</h5>
-        <p>Resolución: ${item.resolucion}</p>
-        <p>Precio: $${item.precio}</p>
+        <h5 class="marca">${item.marca}</h5>
+        <p class="resolucion">Resolución: ${item.resolucion}</p>
+        <p class= "precio">Precio: $${item.precio}</p>
     </div>
     </div>
     `
